2d9eeb608f81024af46db284dbde1a27dc7f5763
E-bit101 <e.fran891@gmail.com>
2026-06-08 19:20:48 -0700

    Basic DE setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,22 +2,28 @@
 
 set -ouex pipefail
 
-### Install packages
-
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Enable COPRs
+dnf5 -y copr enable yalter/niri 
+dnf5 -y copr enable lionheartp/Hyprland
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+### Install packages
+dnf5 install -y \
+
+    fastfetch pipewire wireplumber gvfs \
+
+    fastfetch alacritty thunar \
+    thunar-volman thunar-shares-plugin thunar-archive-plugin \
+
+    niri quickshell sddm hyprland 
+
+# Disable COPRs so they don't end up enabled on the final image
+dnf5 -y copr disable yalter/niri 
+dnf5 -y copr disable lionheartp/Hyprland
 
 #### Example for enabling a System Unit File
 
